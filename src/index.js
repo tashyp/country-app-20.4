d94d9158c2d7ba3e2ffc8fef7da12d777fc02840
Przemek Saja <przemek.saja@gmail.com>
2018-09-01 18:12:10 +0200

    Add sources files with our project

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { Router, hashHistory } from 'react-router';
+import store from './store';
+import DevTools from './DevTools';
+import { getCountries } from '../src/actions/actions-countries';
+import routes from './routes';
+
+render(
+    <Provider store={store}>
+        <Router history={hashHistory}> routes={routes}/>
+    </Provider>,
+    document.getElementById('root')
+);
+
+store.dispatch(getCountries());
